182284a69f5aea12dd566a5cb5b3a65055e67837
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-01-20 17:26:33 +0100

    Fix #13: Implement yatch

--- exercices/exercism-yacht/src/main/java/Yacht.java
+++ exercices/exercism-yacht/src/main/java/Yacht.java
@@ -1,13 +1,42 @@
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 class Yacht {
 
+    private final List<Integer> dices;
+    private final YachtCategory yachtCategory;
+
     Yacht(List<Integer> dices, YachtCategory yachtCategory) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.dices = dices;
+        this.yachtCategory = yachtCategory;
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (yachtCategory.equals(YachtCategory.YACHT)) {
+            Integer last = null;
+            for (Integer dice : dices) {
+                if (last == null) {
+                    last = dice;
+                    continue;
+                }
+                if (!last.equals(dice)) {
+                    return 0;
+                }
+                last = dice;
+            }
+            return 50;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> dicesList = List.of(1, 2, 3, 4, 5, 6);
+        Set<Integer> dicesSet = Set.of(1, 2, 3, 4, 5, 6);
+        Map<String, Integer> dicesMap = Map.of("one", 1, "two", 2, "three", 3);
+        dicesMap.get("one"); // 1
+        dicesMap.get("three"); // 3
+        dicesMap.get("toto"); // null
     }
 
 }
