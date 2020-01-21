44f9e0a9083a655bf9cbaa473cbca8d1479cf5a9
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-01-21 17:31:12 +0100

    Fix #17: Implement yacht

--- exercices/exercism-yacht/src/main/java/Yacht.java
+++ exercices/exercism-yacht/src/main/java/Yacht.java
@@ -2,12 +2,33 @@ import java.util.List;
 
 class Yacht {
 
+    // variable instance (field) (js: propriete)
+    private List<Integer> dices;
+    private YachtCategory yachtCategory;
+
+    // variable locale
     Yacht(List<Integer> dices, YachtCategory yachtCategory) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.dices = dices;
+        this.yachtCategory = yachtCategory;
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int multiplicateur;
+        if (yachtCategory.equals(YachtCategory.FOURS)) {
+            multiplicateur = 4;
+        } else if (yachtCategory.equals(YachtCategory.TWOS)) {
+            multiplicateur = 2;
+        } else {
+            // default
+            multiplicateur = 0;
+        }
+        int compteur = 0;
+        for (int dice : dices) {
+            if (dice == multiplicateur) {
+                compteur++;
+            }
+        }
+        return compteur * multiplicateur;
     }
 
 }
