20094e4e2afa8e1de4a2506a18c3a1d008cec4e6
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-01-27 17:34:00 +0100

    Fix #21: Implement servlet

--- exercices/eugenp-javax-servlets/src/main/java/com/renault/servlets/WelcomeServlet.java
+++ exercices/eugenp-javax-servlets/src/main/java/com/renault/servlets/WelcomeServlet.java
@@ -1,8 +1,43 @@
 package com.renault.servlets;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
 
+@WebServlet(name = "WelcomeServlet", value = "/")
 public class WelcomeServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String header = request.getHeader("Content-Type");
+        String name = request.getParameter("name");
+        if ("application/json".equals(header)) {
+            JsonArrayBuilder elements = Json.createArrayBuilder()
+                    .add(1)
+                    .add(2)
+                    .add(3);
+            String json = Json.createObjectBuilder()
+                    .add("name", name)
+                    .add("date", LocalDate.now().toString())
+                    .add("elements", elements)
+                    .build()
+                    .toString();
+            PrintWriter writer = response.getWriter();
+            writer.write(json);
+        } else {
+            request.setAttribute("name", name);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/welcome.jsp");
+            requestDispatcher.forward(request, response);
+        }
+    }
 
 }
--- exercices/eugenp-javax-servlets/src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ exercices/eugenp-javax-servlets/src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -1,6 +1,14 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <body>
-<div>Hello</div>
+<div>Hello ${name}</div>
+<%
+    String name = (String) request.getAttribute("name");
+    if ("titi".equals(name)) {
+%>
+    <div style="background-color: red; height: 50px; width: 50px"></div>
+<%
+    }
+%>
 </body>
 </html>
