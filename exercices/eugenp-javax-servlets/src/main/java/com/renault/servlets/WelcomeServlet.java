b80170429d67b294fa747b8afd57c19e42d4f33d
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-01-28 14:25:26 +0100

    Fix #23: Implement servlet

--- exercices/eugenp-javax-servlets/src/main/java/com/renault/servlets/WelcomeServlet.java
+++ exercices/eugenp-javax-servlets/src/main/java/com/renault/servlets/WelcomeServlet.java
@@ -1,8 +1,30 @@
 package com.renault.servlets;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "WelcomeServlet", urlPatterns = "/")
 public class WelcomeServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String header = request.getHeader("Content-Type");
+        if ("application/json".equals(header)) {
+            PrintWriter writer = response.getWriter();
+            writer.write("{ name: titi }");
+        } else {
+            String name = request.getParameter("name");
+            request.setAttribute("key", name);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/welcome.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
 
 }
--- exercices/eugenp-javax-servlets/src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ exercices/eugenp-javax-servlets/src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -1,6 +1,18 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
+<head>
+    <style>
+
+    </style>
+</head>
 <body>
-<div>Hello</div>
+<div>Hello ${key}</div>
+<%
+    if ("titi".equals(request.getAttribute("key"))) {
+%>
+    <div style="background-color: red; width: 50px; height: 50px"></div>
+<%
+    }
+%>
 </body>
 </html>
