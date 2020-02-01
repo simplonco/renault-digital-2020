aba49d7f746b1b915ade19cfd40d22f4c73c7664
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-02-01 16:28:33 +0100

    Add sql jpa exercice content

--- exercices/dubreuia-sql-jpa/src/main/java/com/dubreuia/bean/Student.java
+++ exercices/dubreuia-sql-jpa/src/main/java/com/dubreuia/bean/Student.java
@@ -4,6 +4,21 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+/**
+ * Créer une classe étudiant avec les attributs suivants :
+ * <p>
+ * - id (int)
+ * - firstName (String)
+ * - lastName (String)
+ * - birthdate (String)
+ * - note (double)
+ * <p>
+ * Utiliser les annotations suivantes pour paramétrer l'entité :
+ * <p>
+ * - {@link Entity} pour déclarer le lien entre la classe et la table
+ * - {@link Id} pour déclarer la clef primaire
+ * - {@link Column} pour déclarer une colonne
+ */
 @Entity(name = "students")
 public class Student {
 
