5d2c93157d7e6b44f23f109d079ba1a694ddf1f5
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-02-01 16:28:33 +0100

    Add sql jpa exercice content

--- exercices/dubreuia-sql-jpa/README.md
+++ exercices/dubreuia-sql-jpa/README.md
@@ -26,18 +26,20 @@ Installer [sqlite3](https://sqlite.org/download.html) (`sudo apt-get install sql
 
 ### Ajout de contraintes
 
-- Créer table avec contraintes `CREATE TABLE students (id INT PRIMARY KEY, first_name VARCHAR(128), last_name VARCHAR(128) NOT NULL);`
+- Créer table avec contraintes `CREATE TABLE students (id INTEGER PRIMARY KEY, first_name VARCHAR(128), last_name VARCHAR(128) NOT NULL);`
 - Ajouter du contenu
     - `INSERT INTO students VALUES (1, "Alexandre", "DuBreuil");`
     - `INSERT INTO students VALUES (1, "Alexandre", "DuBreuil");`
     - `Error: UNIQUE constraint failed: students.id`
     - `INSERT INTO students VALUES (2, "Alexandre", NULL);`
     - `Error: NOT NULL constraint failed: students.last_name`
+    - `INSERT INTO students VALUES (NULL, "Alexandre", NULL);`
+    - Auto increment
 - Supprimer une table `DROP TABLE students`
 
 ### Types de donnes et modification
 
-- Créer table `CREATE TABLE students (id INT PRIMARY KEY, first_name VARCHAR(128), last_name VARCHAR(128) NOT NULL, birthdate DATE NOT NULL, note DOUBLE);`
+- Créer table `CREATE TABLE students (id INTEGER PRIMARY KEY, first_name VARCHAR(128), last_name VARCHAR(128) NOT NULL, birthdate DATE NOT NULL, note DOUBLE);`
 - Ajouter du contenu
     - `INSERT INTO students VALUES (1, "Alexandre", "DuBreuil", date("1987-11-32"), NULL);`
 - Modifier du contenu
@@ -45,22 +47,26 @@ Installer [sqlite3](https://sqlite.org/download.html) (`sudo apt-get install sql
 - Supprimer du contenu
     - `DELETE FROM students WHERE id = 1;`
 
-### Connecteur
+## Java SQL
 
-https://www.sqlitetutorial.net/sqlite-java/sqlite-jdbc-driver/
+Pour se connecter à la base de donnée, il faut ajouter [le driver JDBC pour SQlite](https://www.sqlitetutorial.net/sqlite-java/sqlite-jdbc-driver/).
+
+- Écrire une méthode `printStudents`, qui affiche le contenu de la table "students"
+- Écrire une méthode `isStudentPresent`, qui retourne `true` si au moins un étudiant avec ce nom et prénom est présent
+- Écrire une méthode `addStudent`, qui ajoute un étudiant à la table "students"
 
 ## JPA
 
-## Outline
+TODO
 
-- first on sql (base)
+## References
+
+- SQL
     - https://sqlite.org/download.html
     - https://www.sqlitetutorial.net/sqlite-java/sqlite-jdbc-driver/
-- root tutorial: https://github.com/RameshMF/JPA-Tutorial
+- JPA https://github.com/RameshMF/JPA-Tutorial
     - first example: https://www.javaguides.net/2018/12/jpa-architecture.html
     - connector: https://www.javaguides.net/2018/12/jpa-obtaining-jpa-database-connection.html
     - crud: https://www.javaguides.net/2018/12/different-ways-to-retrieve-jpa-entity-objects-from-database.html
     - crud: https://www.javaguides.net/2018/12/different-ways-to-update-jpa-entity-objects-into-database.html
     - crud: https://www.javaguides.net/2018/12/jpa-crud-example.html
-
-## Datatypes
--- exercices/dubreuia-sql-jpa/src/main/java/com/dubreuia/persistance/Jpa.java
+++ exercices/dubreuia-sql-jpa/src/main/java/com/dubreuia/persistance/Jpa.java
@@ -10,12 +10,68 @@ import javax.persistence.Persistence;
 public class Jpa {
 
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("DATABASE");
+        Student student;
+
+        student = getStudentsWithId(10);
+        if (student == null) {
+            insertStudent(new Student(10, "Barack", "Obama", "1961-08-04", 5.7));
+        } else {
+            System.out.println(student);
+        }
+
+        student = getStudentsWithId(20);
+        if (student == null) {
+            insertStudentNoCommit(new Student(20, "Barack", "Obama", "1961-08-04", 5.7));
+        } else {
+            System.out.println(student);
+        }
+
+        student = getStudentsWithId(30);
+        if (student == null) {
+            insertStudentNoCommit(new Student(30, "Barack", "Obama", "1961-08-04", 5.7));
+        } else {
+            System.out.println(student);
+        }
+    }
+
+    private static Student getStudentsWithId(int id) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("database");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Student student = entityManager.find(Student.class, id);
+        try {
+            return student;
+        } finally {
+            entityManagerFactory.close();
+        }
+    }
+
+    private static void insertStudent(Student student) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("database");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
-        System.out.println(entityManager.find(Student.class, 1));
+        entityManager.persist(student);
         transaction.commit();
+        entityManagerFactory.close();
+    }
+
+    private static void insertStudentNoCommit(Student student) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("database");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(student);
+        entityManagerFactory.close();
+    }
+
+    private static void insertStudentCommitAndRollback(Student student) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("database");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(student);
+        transaction.commit();
+        transaction.rollback();
         entityManagerFactory.close();
     }
 
