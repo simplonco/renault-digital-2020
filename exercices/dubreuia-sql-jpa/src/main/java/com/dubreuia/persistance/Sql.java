41f17b4e82e4f1c498ed87e212995cd4aedd246f
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-02-03 14:25:01 +0100

    Add SQL exercice content

--- exercices/dubreuia-sql-jpa/src/main/java/com/dubreuia/persistance/Sql.java
+++ exercices/dubreuia-sql-jpa/src/main/java/com/dubreuia/persistance/Sql.java
@@ -34,6 +34,21 @@ public class Sql {
      */
     private static void printStudents()
             throws SQLException {
+        try (var connection = DriverManager.getConnection(URL)) {
+            var preparedStatement = connection.prepareStatement("SELECT * FROM students");
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                var id = resultSet.getInt("id");
+                var first_name = resultSet.getString("first_name");
+                var last_name = resultSet.getString("last_name");
+                var birthdate = resultSet.getString("birthdate");
+                var note = resultSet.getString("note");
+                if (note != null) {
+                    var noteDouble = Double.parseDouble(note);
+                }
+                System.out.println(id + ", " + first_name + ", " + last_name + ", " + birthdate + ", " + note);
+            }
+        }
     }
 
     /**
@@ -47,6 +62,15 @@ public class Sql {
      */
     private static void addStudent(String firstName, String lastName, String birthdate, Double note)
             throws SQLException {
+        try (var connection = DriverManager.getConnection(URL)) {
+            String statement = "INSERT INTO students (first_name, last_name, birthdate, note) VALUES (?, ?, ?, ?)";
+            var preparedStatement = connection.prepareStatement(statement);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, birthdate);
+            preparedStatement.setDouble(4, note);
+            preparedStatement.executeUpdate();
+        }
     }
 
     /**
@@ -60,7 +84,12 @@ public class Sql {
      */
     private static boolean isStudentPresent(String firstName, String lastName)
             throws SQLException {
-        return false;
+        try (var connection = DriverManager.getConnection(URL)) {
+            String requeteSQL = String.format("SELECT count(*) FROM students WHERE first_name = '%s' AND last_name = '%s'", firstName, lastName);
+            var preparedStatement = connection.prepareStatement((requeteSQL));
+            var resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            return resultSet.getInt(1) > 0;
+        }
     }
-
 }
