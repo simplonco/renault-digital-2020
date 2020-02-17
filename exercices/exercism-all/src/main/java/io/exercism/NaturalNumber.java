fb1aa5386113e77aeba49bde012394379b256112
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-02-17 11:06:58 +0100

    Add natural number exercism

---
+++ exercices/exercism-all/src/main/java/io/exercism/NaturalNumber.java
@@ -0,0 +1,18 @@
+package io.exercism;
+
+class NaturalNumber {
+
+    public NaturalNumber(int number) {
+    }
+
+    public Classification getClassification() {
+        return null;
+    }
+
+    enum Classification {
+
+        DEFICIENT, PERFECT, ABUNDANT
+
+    }
+
+}
