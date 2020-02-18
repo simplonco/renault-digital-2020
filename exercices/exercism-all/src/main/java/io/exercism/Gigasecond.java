2ec722ef903b93a6b30e85ed1e2ee0d7dbf5f61c
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-02-18 09:46:05 +0100

    Add exercism

--- exercices/exercism-all/src/main/java/io/exercism/Gigasecond.java
+++ exercices/exercism-all/src/main/java/io/exercism/Gigasecond.java
@@ -3,18 +3,22 @@ package io.exercism;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
+
 class Gigasecond {
 
+    private LocalDateTime moment;
+
     Gigasecond(LocalDate moment) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.moment = moment.atTime(0, 0);
     }
 
     Gigasecond(LocalDateTime moment) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.moment = moment;
     }
 
     LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return moment.plus(1_000_000_000, SECONDS);
     }
 
 }
--- exercices/exercism-all/src/main/java/io/exercism/NaturalNumber.java
+++ exercices/exercism-all/src/main/java/io/exercism/NaturalNumber.java
@@ -2,16 +2,38 @@ package io.exercism;
 
 class NaturalNumber {
 
+    private int number;
+
     public NaturalNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+        this.number = number;
+    }
+
+    private int getAliqotSum() {
+        int aliqotSum = 0;
+        for (int i = 1; i < number - 1; i++) {
+            if ((number % i) == 0) {
+                aliqotSum += i;
+            }
+        }
+        return aliqotSum;
     }
 
     public Classification getClassification() {
-        return null;
+        if (getAliqotSum() == number) {
+            return Classification.PERFECT;
+        } else if (getAliqotSum() < number) {
+            return Classification.DEFICIENT;
+        } else {
+            return Classification.ABUNDANT;
+        }
     }
 
     enum Classification {
 
-        DEFICIENT, PERFECT, ABUNDANT
+        DEFICIENT, PERFECT, ABUNDANT;
 
     }
 
