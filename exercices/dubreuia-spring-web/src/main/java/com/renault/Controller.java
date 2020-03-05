facd3bbc65efbbe277ca92cb7265fcf72db5647f
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-03-05 18:12:13 +0100

    Add simple spring web example

---
+++ exercices/dubreuia-spring-web/src/main/java/com/renault/Controller.java
@@ -0,0 +1,27 @@
+package com.renault;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin
+public class Controller extends HttpServlet {
+
+    @GetMapping("/persons")
+    public List<PersonDto> get() {
+        return List.of(
+                new PersonDto("Emmanuel", "Macron"),
+                new PersonDto("François", "Hollande"),
+                new PersonDto("Nicolas", "Sarkozy"),
+                new PersonDto("Jacques", "Chirac"),
+                new PersonDto("François", "Mitterrand")
+        );
+    }
+
+}
