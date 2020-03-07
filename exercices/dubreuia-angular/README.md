9dcbbd57e1f99f7e0c9300cf21c24f89c04aa318
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-03-07 16:30:30 +0100

    Add angular front

---
+++ exercices/dubreuia-angular/README.md
@@ -0,0 +1,17 @@
+# Angular
+
+- `npm install -g @angular/cli@1.7.4`
+- `ng new people-front` (see dubreuia-spring-web TODO README)
+- `cd people-front`
+- `npm start` go to http://localhost:4200/
+- `ng generate class person`
+- `ng generate service person-service`
+
+## Archive
+
+- `ng new --minimal weather-front`
+- develop app to fetch information from
+    - https://github.com/public-apis/public-apis#weather
+    - https://www.metaweather.com/api/location/search/?query=london
+- `npm start`
+
---
+++ exercices/dubreuia-angular/people-front/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { PersonListComponent } from './person-list.component';
+import { PersonService } from './person-service.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'persons', component: PersonListComponent },
+    ])
+  ],
+  providers: [PersonService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+console.log("fu3")
