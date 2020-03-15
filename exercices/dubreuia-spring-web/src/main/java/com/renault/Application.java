32a0f729dff073e6e31797a480a6b1d210927a3b
Alexandre DuBreuil <code@alexandredubreuil.com>
2020-03-15 17:02:12 +0100

    Remove spring data support for web project

--- exercices/dubreuia-spring-web/src/main/java/com/renault/Application.java
+++ exercices/dubreuia-spring-web/src/main/java/com/renault/Application.java
@@ -1,7 +1,6 @@
 package com.renault;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
